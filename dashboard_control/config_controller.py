c689bf214791dc77b993d34632eaaabc2d645915
shay.perp <shayperp@rafael.co.il>
2022-09-20 22:34:42 +0300

    controller ui / streamlit v 2

--- dashboard_control/config_controller.py
+++ dashboard_control/config_controller.py
@@ -1,24 +1,58 @@
-import os
 
 
-def func_to_execute(func_name=None):
-    if func_name is not None:
-        tests_names = {
-            'sharizi tiff': 'shaziri_tiff_overseer',
-            'different_zoom_levels': 'test_different_zoom_levels.test_zoom_level()',
-            'failure_exist_product_manual_ingestion':
-                'test_failure_exists_product_manual_ingestion.test_exists_product_manual_ingestion()',
-            'failure_illegal_zoom_level_limit': 'test_failure_illegal_zoom_level_limit.test_illegal_zoom()',
-            'test_failure_missing_files': 'test_failure_missing_files.test_missing_files()',
-            'ingestion_discrete': 'test_ingestion_discrete.test_manual_discrete_ingest()',
-            'invalid_imagery_data': 'test_invalid_imagery_data.test_invalid_data()',
-            'manual_geopackage_ingestion': 'test_manual_geopackage_ingestion.test_manual_ingestion_geopackage()',
-            'parallel_ingestion_workers': 'test_parallel_ingestion_workers.test_parallel_ingestion()',
-            'priority_change': 'test_priority_change.test_priority_change()',
-            'watch_ingestion_discrete': 'test_watch_ingestion_discrete.test_watch_discrete_ingest()',
-        }
-        return exec(tests_names[func_name])
-    return None
+def different_zoom_levels():
+    return "test_different_zoom_levels.test_zoom_level()"
+
+
+def failure_exist_product_manual_ingestion():
+    return " test_failure_exists_product_manual_ingestion.test_exists_product_manual_ingestion()"
+
+
+def test_failure_illegal_zoom_level_limit():
+    return "test_failure_illegal_zoom_level_limit.test_illegal_zoom()"
+
+
+def test_failure_missing_files():
+    return "test_failure_missing_files.test_missing_files()"
+
+
+def ingestion_discrete():
+    return "test_ingestion_discrete.test_manual_discrete_ingest()"
+
+
+def invalid_imagery_data():
+    return "test_invalid_imagery_data.test_invalid_data()"
+
+
+def manual_geopackage_ingestion():
+    return "test_manual_geopackage_ingestion.test_manual_ingestion_geopackage()"
+
+
+def parallel_ingestion_workers():
+    return "test_parallel_ingestion_workers.test_parallel_ingestion()"
+
+
+def priority_change():
+    return "test_priority_change.test_priority_change()"
+
+
+def watch_ingestion_discrete():
+    return "test_watch_ingestion_discrete.test_watch_discrete_ingest()"
+
+
+tests_names = {
+    'sharizi tiff': 'shaziri_tiff_overseer',
+    'different_zoom_levels': different_zoom_levels(),
+    'failure_exist_product_manual_ingestion': failure_exist_product_manual_ingestion(),
+    'failure_illegal_zoom_level_limit': test_failure_illegal_zoom_level_limit(),
+    'test_failure_missing_files': test_failure_missing_files(),
+    'ingestion_discrete': ingestion_discrete(),
+    'invalid_imagery_data': invalid_imagery_data(),
+    'manual_geopackage_ingestion': manual_geopackage_ingestion(),
+    'parallel_ingestion_workers': parallel_ingestion_workers(),
+    'priority_change': priority_change(),
+    'watch_ingestion_discrete': watch_ingestion_discrete(),
+}
 
 
 list_test_name = [
@@ -32,12 +66,10 @@ list_test_name = [
     'manual_geopackage_ingestion',
     'parallel_ingestion_workers',
     'priority_change',
-    'watch_ingestion_discrete']
+    'watch_ingestion_discrete'
+]
 
 test_conf = {'PYTHONUNBUFFERED': 1,
              'CONF_FILE': "/home/shayperp/Desktop/auto-int/automation-ingestion-test/server_automation/configuration"
                           "/configuration.json",
              }
-
-os.environ["PYTHONUNBUFFERED"] = test_conf['PYTHONUNBUFFERED']
-os.environ["CONF_FILE"] = test_conf['CONF_FILE']
--- dashboard_control/controller.py
+++ dashboard_control/controller.py
@@ -1,18 +1,29 @@
-from dashboard_control.config_controller import list_test_name, func_to_execute
 import streamlit as st
+from config_controller import list_test_name, tests_names
+from server_automation.tests import *
 
-
-st.title('Welcom to test app')
+st.title('Dashboard tests run')
 option = st.selectbox(
     'Which test u want to run ?', tuple(list_test_name)
     )
 st.text_input("Enter parameters if u need ", "CONF_FILE=")
 
-button = st.button("my button")
+st.write('Test to run:', option)
+
+button = st.button("Run Test ")
+
 if button:
-    st.success('This is a success message!', icon="✅")
-    st.warning(option, icon="⚠️")
-    func_to_execute(option)
+    show_log = True
+    try:
+        exec(tests_names[option])
+    except Exception as e:
+        show_log = False
+        st.warning("Exception", icon="⚠️")
+        st.code(str(e), language="python")
+    if show_log:
+        st.success('This is a success message!', icon="✅")
+        st.code("OK", language="python")
+
 
 st.write('You selected:', option)
 
