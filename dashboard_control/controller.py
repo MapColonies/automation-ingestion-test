687116da71250f1cf4d529fc7f8c38fbec38b6ba
shay.perp <shayperp@rafael.co.il>
2022-09-21 16:48:46 +0300

    controller Stramlit .

--- dashboard_control/controller.py
+++ dashboard_control/controller.py
@@ -1,29 +1,60 @@
+import glob
+import os
+from multiprocessing import Process
 import streamlit as st
-from config_controller import list_test_name, tests_names
-from server_automation.tests import *
+import importlib
+import time
 
+MAIN_PATH = 'server_automation'
+CONF_FP = f"{MAIN_PATH}/configuration/configuration.json"
+LOGS = "logs.log"
+
+files = glob.glob(f'server_automation/tests/*py')
 st.title('Dashboard tests run')
-option = st.selectbox(
-    'Which test u want to run ?', tuple(list_test_name)
-    )
-st.text_input("Enter parameters if u need ", "CONF_FILE=")
+path = st.selectbox(
+    'Which test u want to run ?', tuple(files)
+)
+options = 'Default', 'Custom'
+config_file = st.radio('Config File:', options=options)
 
-st.write('Test to run:', option)
+fp = CONF_FP if config_file == options[0] else st.text_input("Enter parameters if u need ")
 
-button = st.button("Run Test ")
+st.write('Test to run:', path)
 
-if button:
-    show_log = True
-    try:
-        exec(tests_names[option])
-    except Exception as e:
-        show_log = False
-        st.warning("Exception", icon="⚠️")
-        st.code(str(e), language="python")
-    if show_log:
-        st.success('This is a success message!', icon="✅")
-        st.code("OK", language="python")
+if os.path.exists(LOGS):
+    os.remove(LOGS)
 
+if st.button("Run Test "):
+    process = Process(target=os.system, args=(f"CONF_FILE={fp} py.test {path} >{LOGS}",))
+    process.start()
+    with st.expander('Logs', expanded=True):
+        placeholder = st.empty()
+        with st.empty():
+            while process.is_alive():
+                time.sleep(2)
+                if os.path.exists(LOGS):
+                    with open(LOGS, 'r') as l:
+                        st.code(l.read(), language="python")
+        # Clear all those elements:
+        placeholder.empty()
 
-st.write('You selected:', option)
+    # st.write(os.system(f"CONF_FILE={fp} py.test {path} >{LOGS}"))
 
+# if button:
+#     show_log = True
+#     try:
+#         imp = "server_automation.tests.test_"+str(option)
+#         print(imp)
+#         importlib.import_module(imp)
+#         st.write(option)
+#         st.write(tests_names[option])
+#         exec(tests_names[option])
+#     except Exception as e:
+#         show_log = False
+#         st.warning("Exception", icon="⚠️")
+#         st.code(str(e), language="python")
+#     if show_log:
+#         st.success('This is a success message!', icon="✅")
+#         st.code("OK", language="python")
+
+# st.write('You selected:', option)
